bbbcbc1450f2dd3f9b63426800af87b7dd0e07fa
derselbst <tom.mbrt@googlemail.com>
2015-02-09 19:19:13 +0100

    start using isCollision for collision detection

--- breakout/logic.c
+++ breakout/logic.c
@@ -9,23 +9,83 @@
 #include "paddle.h"
 #include "wall.h"
 
+// evaluate each operand exactly once, otherwise bad results if the operand has side effects
+#define max(a,b) \
+	    ({ \
+			typeof (a) _a = (a); \
+			typeof (b) _b = (b); \
+			_a > _b ? _a : _b;   \
+		})
+
+#define min(a,b) \
+	    ({ \
+			typeof (a) _a = (a); \
+			typeof (b) _b = (b); \
+			_a < _b ? _a : _b;   \
+		})
+
+#define swap(a,b) \
+		({ \
+			typeof (a) _a = (a); \
+			typeof (b) _b = (b); \
+			(b) = _a;            \
+			(a) = _b;            \
+		})
+		
+/**
+ * @brief checks if a collision in one dimension occurred
+ *
+ * checks if a collision in one dimensional space occurred
+ *
+ * @param upperBound the coordinate of the upper included bound
+ * @param lowerBound the coordinate of the lower included bound
+ * @param pos the currenz position of to object to test
+ */
+inline bool isCollision(uint8_t upperBound, uint8_t lowerBound, const uint8_t pos)
+{
+	if (upperBound < lowerBound)
+	{
+		swap(upperBound, lowerBound);
+	}
+	return lowerBound <= pos && pos <= upperBound; // lowerBound <= pos <= upperBound
+}
+
+
 static void collisionWall(struct _ball *const ball, const struct _wall *const wall)
 {
-    if(ball->x-BALLSIZE == wall->left+1)
-    {
-        // collision with left wall
-        ball->dx=1;
-    }
-    else if(ball->x+BALLSIZE == wall->right-1)
-    {
+  
+  if(isCollision(wall->left + BALLSIZE, wall->left, ball->x))
+  {
+    // collision with left wall
+    ball->dx=1;
+  }
+  else if(isCollision(wall->right, wall->right - BALLSIZE, ball->x))
+  {
         // collision with right wall
         ball->dx=-1;
-    }
-
-    if(ball->y-BALLSIZE == wall->top+1)
-    {
-        ball->dy=1;
-    }
+  }
+  
+  if(isCollision(wall->top + BALLSIZE, wall->top, ball->y))
+  {
+    ball->dy=1;
+  }
+  
+  
+//     if(ball->x-BALLSIZE == wall->left+1)
+//     {
+//         // collision with left wall
+//         ball->dx=1;
+//     }
+//     else if(ball->x+BALLSIZE == wall->right-1)
+//     {
+//         // collision with right wall
+//         ball->dx=-1;
+//     }
+// 
+//     if(ball->y-BALLSIZE == wall->top+1)
+//     {
+//         ball->dy=1;
+//     }
 }
 
 static bool collisionPaddle(struct _ball *const ball, const struct _paddle *const paddle)
